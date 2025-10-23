c2b513d59d0da2ecba16293ea5e6e3e334fbd353
Angus <joshua.angus@kingsway.school.nz>
2025-10-24 09:59:30 +1300

    Error Checking Version 1.4
    Made it better

--- error_checking.py
+++ error_checking.py
@@ -1,77 +1,95 @@
-# Error checking 
-# Author: Josh
-# Date 22/10/2025
-# Version 1
+#Coffe Program
+#Creating an input system for our coffee program
+#Juergen Lier
+#Date: 10/10/2025
 
-#Code that tests whether a valid input is given (v.1)
-'''done = False # Boolean variable set to False
-while not done:
-    num = int(input("Please enter your value"))
-    done = True
+# Version: 1
+# TODO: Ask the user if they like coffee    
+
+# Ask  the user whether they like coffee or not
+'''like_coffee = input("Do you like coffee")
+print(f'Your answer was "{like_coffee}".')
+if like_coffee == "yes" or "Yes":
+    print("This is great! I like coffee too.")
+else:
+    print("you are missing out! Why not give it a try?")'''
+# Version 2
+# While loop
+'''keep_going = ""
+while keep_going == "":
+    like_coffee = input("Do you like coffee")
     
-print(f"The number you entered is {num}")'''
-
-#Code that tests whether a valid input is given (v1.1)
-# Use the try and except method to catch errors
-'''done = False
-while not done:
-    try: #This method tries for a valid input
-        num = int(input("Please enter a number: "))
-        done = True
+    if like_coffee == "yes" or like_coffee == "Yes" or like_coffee == "y" or like_coffee == "Y":
+        print("This is great! I like coffee too.")
+        keep_going = "finished"
+    
+    elif like_coffee == "no" or like_coffee == "No" or like_coffee == "N" or like_coffee == "n":    
+        print("you are missing out! Why not give it a try?")
+        keep_going = "finished"
         
-    except ValueError:
-        print("That is not a valid float number. \n")
+    else:
+        print("I don't understand")'''
 
-print(f"The number you entered is {num}")'''
+
+# Version 3
+# Making the program more Pythonic
+'''keep_going = ""
+while keep_going == "":
+    # Convert answer to lower case using .lower
+    like_coffee = input("Do you like coffee?\n"). lower()
+    if like_coffee == "yes" or like_coffee == "y":
+        print("That's great I like coffee too")
     
-# Code that tests whether a valid input is given (v1.2)
-# Create a function to call everytime I ask the  user for a nummber .A 
-# A function is a chunk of code that does something
-# Functions can be used over and over again.
-# To use a function (call it), write out its name with parantheses at the end
-# To create a function. strat with the word def
-
-
-'''def test_int_num():
-    done = False
-    while not done:
-        try: #This method tries for a valid input
-            num = int(input("Please enter a number: "))
-            done = True
+    elif like_coffee == "no" or like_coffee or like_coffee == "n":
+        print("You are missing out! Why not give it a try?")
+        
+        like_tea = input("Do you like tea insted?").upper()
+        if like_tea == "YES" or like_tea == "Y":
+            print("Good for you. Give coffee another try :)")
+        
+        elif like_tea == "NO" or like_tea == "N":
+            print("I am sorry. Thats all I have for now.")
+        
+        else:
+            print("I dont understand please answer with either Yes or No")
             
-        except ValueError:
-            print("That is not a valid float number. \n")
-
-    print(f"The number you entered is {num}.")
+    else:
+            print("I dont understand please answer with either Yes or No")
     
-# Main routine 
-test_int_num() # This calls the function so that we can use it'''
-
-#cODE THAT TESTS WHETHER A VALID INPUT IS GIVEN (v1.3)
-#USE THE FUNCTION PARAMETERS
-
-def test_int(question): # 'question' is a placeholder
-    done = False
-    while not done:
-        error = "That is not a valid number"
-        print(question)
-        try: #This method tries for a valid input
-            num = int(input())
-            done = True
+    keep_going = input("press <ENTER> to continue, or any other key to quit. Thanks!\n")'''
+    
+# Code that tests whether a vailid input iss given (v1.4)
+# Use the function parameters to make my code more pythonuc.
+    
+def valid_num(question, low, high):
+    error = f"Woops, that is not an interger between {low} and {high}."
+    while True:
+        try:
+            response = int(input(question))
+            if low <= response <= high:
+                break # This ends the loop
             
+            else:
+                print(f"{error}\n")
         except ValueError:
             print(error)
+    return response # This makes the response available toe be used
 
-    return(num) # gives back the value of num so that I can use it outside of the function.
-    
-    
-# Main routine
-num1 = test_int("please enter your first number:")
-print(f"Your first number you entered is {num1}")
+if __name__ == "__main__":
+    num_1 = valid_num("Enter a number between 1 - 10:", 1,10)
+    print(f"You entered {num_1}\n")
 
-num2 = test_int('Please enter your second number:')
-print(f"The second number you entered is {num2}")
+    num_2 = valid_num("Enter a number between 15 - 25:",15,25)
+    print(f"You entered {num_2}\n")
 
-sum = num1 + num2
-print(f"Your numbers added together equal to {sum}")
-    
+    num_3 = valid_num("Enter a number between 30 - 45:",30,45)
+    print(f"You entered {num_3}\n")
+
+    num_4 = valid_num("Enter a number between 50 - 65:",50,65)
+    print(f"You entered {num_4}\n")
+
+    multiply = num_1 * num_2 * num_3 * num_4
+    print(f"Your three numbers multiplied together are equal to {multiply}\n")
+
+    sum = num_1 + num_2 + num_3
+    print(f"The total of {num_1}, {num_2} and {num_3} is {sum}.\n")
